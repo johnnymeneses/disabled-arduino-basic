09a0f9e1fdf20572c0a8c3419386e1d193c4bb45
johnnymeneses <bilbo_vader@outlook.com>
2021-09-26 15:32:42 -0300

    servo Motor

---
+++ servoMotor/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <Servo.h>
+ 
+#define SERVO 6 // Porta Digital 6 PWM
+ 
+Servo s; // Variável Servo
+int pos; // Posição Servo
+ 
+void setup ()
+{
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(0); // Inicia motor posição zero
+}
+ 
+void loop()
+{
+  for(pos = 0; pos < 90; pos++)
+  {
+    s.write(pos);
+  delay(10);
+  }
+delay(50000);
+ /* for(pos = 90; pos >= 0; pos--)
+  {
+    s.write(pos);
+    delay(10);
+  }*/
+}
