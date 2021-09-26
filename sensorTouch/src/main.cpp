b81e02931c4488c129c3decddc6213a0835deae7
johnnymeneses <bilbo_vader@outlook.com>
2021-09-26 15:33:35 -0300

    sensorTouch

---
+++ sensorTouch/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+// constants won't change. They're used here to set pin numbers:
+const int SENSOR_PIN = 7; // the Arduino's input pin that connects to the sensor's SIGNAL pin 
+const int led = 11;
+
+// Variables will change:
+int lastState = LOW;      // the previous state from the input pin
+int currentState;         // the current reading from the input pin
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // initialize the Arduino's pin as aninput
+  pinMode(SENSOR_PIN, INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // read the state of the the input pin:
+  currentState = digitalRead(SENSOR_PIN);
+
+  // digitalWrite(led, HIGH);
+  if(lastState == LOW && currentState == HIGH)
+    {
+      Serial.println("The sensor is touched");
+      digitalWrite(led, HIGH);
+    }
+    
+  // save the the last state
+  lastState = currentState;
+}
